bf6edf6782218e2a19a01f4018f249517a36bbe8
tjallen27 <tjallen91@hotmail.co.uk>
2018-02-01 08:44:39 +0000

    Set index route to app.js

--- app.js
+++ app.js
@@ -2,4 +2,21 @@
 // publishable:
 
 const express = require('express');
-const stripe = require('stripe')('secret_key');
+const stripe = require('stripe')('sk_test_gsF7ubnlEZsQvxDjBiYPv8xy');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+//  handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+// index route
+app.get('/', (req, res)=>{
+  res.render('index');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
